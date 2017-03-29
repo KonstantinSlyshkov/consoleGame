ef39c8106d2832b4fc361090058f84a7763ee915
Kostiantyn Slyshkov <kostiantyn_slyshkov@epam.com>
2017-03-29 14:00:14 +0300

    asfasf

--- script.sh
+++ script.sh
@@ -9,14 +9,14 @@ do
 OUTCOME=$(curl -s "https://circleci.com/api/v1.1/project/github/konstantinslyshkov/webApplication/${BUILD_NUMBER}?circle-token=79420eb9943f598fcc19f7b7becf408031d39ae0" | sed -n 's/\"outcome\"[[:space:]]:[[:space:]]\"//p' | sed s'/\",$//' | tail -n 1)
 echo $OUTCOME test
 if [[ $OUTCOME =~ .*failed.* ]]; then
-	echo build number $BUILD_NUMBER failed; 
-	break;
+	echo ERROR: Functional tests build number $BUILD_NUMBER failed; 
+	exit 1;
 elif [[ $OUTCOME =~ *success.* ]]; then
-    echo build number $BUILD_NUMBER completed succesfully;
+    echo INFO: Functional tests build number $BUILD_NUMBER completed succesfully;
 	break;
 elif [[ $OUTCOME == *cancelled.* ]]; then
-    echo build number $BUILD_NUMBER cancelled;
-	break;
+    echo WARNING: Functional tests build number $BUILD_NUMBER cancelled;
+	exit 126;
 else 
 	sleep 5;
 fi
