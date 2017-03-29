51da75592069d1800bf5507a4afc8452ace7da5e
Kostiantyn Slyshkov <kostiantyn_slyshkov@epam.com>
2017-03-29 13:40:41 +0300

    asfasf

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BUILD_NUMBER=$(curl -v -X POST https://circleci.com/api/v1.1/project/github/konstantinslyshkov/webApplication/tree/master?circle-token=79420eb9943f598fcc19f7b7becf408031d39ae0 | grep -m 2 -Po '\"build_num\" : \K[0-9]+' | tail -n 1 )
+BUILD_NUMBER=$(curl -v -s -X POST https://circleci.com/api/v1.1/project/github/konstantinslyshkov/webApplication/tree/master?circle-token=79420eb9943f598fcc19f7b7becf408031d39ae0 | grep -m 2 -Po '\"build_num\" : \K[0-9]+' | tail -n 1 )
 
 echo build number $BUILD_NUMBER of tests started 
 
@@ -10,10 +10,10 @@ echo $OUTCOME test
 if [[ $OUTCOME =~ .*failed.* ]]; then
 	echo build number $BUILD_NUMBER failed; 
 	break;
-elif [ $OUTCOME == *success.* ]; then
+elif [ $OUTCOME =~ *success.* ]; then
     echo build number $BUILD_NUMBER completed succesfully;
 	break;
-elif [ $OUTCOME == *cancelled.* ]; then
+elif [ $OUTCOME =~ *cancelled.* ]; then
     echo build number $BUILD_NUMBER cancelled;
 	break;
 else 
