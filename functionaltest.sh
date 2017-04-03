17216674b39feb31fa19d7feac33f29b052f0520
Kostiantyn Slyshkov <kostiantyn_slyshkov@epam.com>
2017-04-03 14:29:33 +0300

    test commit

--- functionaltest.sh
+++ functionaltest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'Waiting for instances to be up and running'
-sleep 2m
+sleep 10s
 token=$1
 BUILD_NUMBER=$(curl -v -X POST "https://circleci.com/api/v1.1/project/github/KonstantinSlyshkov/getftpfeedswithnotifications/tree/master?circle-token=${token}" | grep -m 2 -Po '\"build_num\" : \K[0-9]+' | tail -n 1 )
 
@@ -8,7 +8,7 @@ echo build number $BUILD_NUMBER of tests started
 
 while true
 do
-OUTCOME=$(curl -s "https://circleci.com/api/v1.1/project/github/ConstellationBrands/KonstantinSlyshkov/getftpfeedswithnotifications/${BUILD_NUMBER}?circle-token=${token}" | sed -n 's/\"outcome\"[[:space:]]:[[:space:]]\"//p' | sed s'/\",$//' | tail -n 1)
+OUTCOME=$(curl -s "https://circleci.com/api/v1.1/project/github/KonstantinSlyshkov/getftpfeedswithnotifications/${BUILD_NUMBER}?circle-token=${token}" | sed -n 's/\"outcome\"[[:space:]]:[[:space:]]\"//p' | sed s'/\",$//' | tail -n 1)
 if [[ $OUTCOME =~ .*failed.* ]]; then
 	echo ERROR: Functional tests build number $BUILD_NUMBER failed; 
 	exit 1;
